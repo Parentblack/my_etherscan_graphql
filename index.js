0728182c5ac2e8502cf82e827e93c581b4bea579
Parentblack <145247578+Parentblack@users.noreply.github.com>
2023-11-21 19:04:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const { ApolloServer } = require("apollo-server"); // import ApolloServer from apollo-server
+const { importSchema } = require("graphql-import"); // import schema from graphql-import
+const EtherDataSource = require("./datasource/ethDatasource"); // import EtherDataSource class
+const typeDefs = importSchema("./schema.graphql"); // import schema from schema.graphql file
+
+require("dotenv").config(); // load environment variables
+
+const resolvers = {
+  Query: {
+    etherBalanceByAddress: (root, _args, { dataSources }) => // resolver to get ether balance 
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    totalSupplyOfEther: (root, _args, { dataSources }) => // resolver to get total ether supply
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    latestEthereumPrice: (root, _args, { dataSources }) => // resolver to get latest eth price
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (root, _args, { dataSources }) => // resolver to get block confirmation time
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({ // create ApolloServer instance
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(), // instantiate EtherDataSource
+  }), 
+});
+
+server.timeout = 0; 
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`); // log message when server starts
+});
